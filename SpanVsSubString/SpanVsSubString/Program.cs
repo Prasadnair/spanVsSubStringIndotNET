36d75752755f67b73083f50d3b7c4ec79f4e0b20
Prasadnair <prasadnair44@gmail.com>
2024-08-11 15:07:45 -0400

    Span vs SubString

---
+++ SpanVsSubString/SpanVsSubString/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using SpanVsSubString;
+
+Console.WriteLine("Substring Vs Span!");
+
+var summary = BenchmarkRunner.Run<StringManipulationBenchmark>();
---
+++ SpanVsSubString/SpanVsSubString/StringManipulationBenchmark.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace SpanVsSubString
+{
+    [MemoryDiagnoser]
+    public class StringManipulationBenchmark
+    {
+        private const string Data = "This is a sample string for demonstrating Span and Substring performance in .NET.";
+
+        [Benchmark]
+        public string UsingSubstring()
+        {
+            return Data.Substring(10, 6);
+        }
+
+        [Benchmark]
+        public ReadOnlySpan<char> UsingSpan()
+        {
+            return Data.AsSpan().Slice(10, 6);
+        }
+
+        [Benchmark]
+        public string SpanToString()
+        {
+            return Data.AsSpan().Slice(10, 6).ToString();
+        }
+    }
+}
